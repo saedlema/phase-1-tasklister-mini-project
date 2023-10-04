dd09153aabb951af742250428a9645ce5d4c13a2
saedc <saedcl1@gmail.com>
2023-10-04 17:20:13 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+const descriptionElement = document.getElementById("create-task-form")
+descriptionElement.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+  const descriptionElement = document.getElementById("new-task-description")
+  
+  const descriptionElementInput = descriptionElement.value
+
+  const li = document.createElement("li")
+  li.textContent = descriptionElementInput
+
+  const taskList = document.getElementById("tasks")
+  taskList.appendChild(li)
+
+  const button = document.createElement("button")
+  button.textContent = "x"
+
+  li.appendChild(button)
+
+  button.addEventListener("click", () => {
+    taskList.removeChild (li)
+  })
+
+
+
+
+
+  
+
+
+
+
+
+})
+}); 
